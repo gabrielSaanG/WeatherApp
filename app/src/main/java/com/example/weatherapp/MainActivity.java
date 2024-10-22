cadf9fbab76132067cd34e1fe778efda83ef519b
Gabriel Aragão Sanches <gabriel.sanches@edu.unipar.br>
2024-10-22 07:36:25 -0300

    creating navigation bar

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,11 +3,44 @@ package com.example.weatherapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.weatherapp.databinding.ActivityMainBinding;
 
 public class MainActivity  extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.navHome:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.location:
+                    replaceFragment(new LocationFragment());
+                    break;
+                case R.id.about:
+                    replaceFragment(new AboutFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
