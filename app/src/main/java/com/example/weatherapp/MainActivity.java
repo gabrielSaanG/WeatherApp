c5fd6f46ee1f86c54b9285dfe6664b41112b5110
Gabriel Aragão Sanches <gabriel.sanches@edu.unipar.br>
2024-10-27 16:09:09 -0300

    creating home cardView and RecyclerView.

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.weatherapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import com.example.weatherapp.MyAdapter.*;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.weatherapp.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity  extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    String txtJson;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -29,7 +49,7 @@ public class MainActivity  extends AppCompatActivity {
 
 
         bottomNavigationView.setOnItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) item -> {
-            switch(item.getItemId()){
+            switch (item.getItemId()) {
                 case R.id.navHome:
 
                     break;
@@ -45,12 +65,28 @@ public class MainActivity  extends AppCompatActivity {
             }
             return false;
         });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        List<Item> items = new ArrayList<Item>();
+
+        items.add(new Item("Roma", "Itália", "13 C", "09:33", R.drawable.icon_cloud));
+
+        items.add(new Item("Atenas", "Grécia", "19 C", "10:22", R.drawable.icon_cloud));
+
+        items.add(new Item("São Paulo", "São Paulo", "22 C", "12:45", R.drawable.icon_cloud));
+
+        String url = "https://api.hgbrasil.com/weather?woeid=461662";
+
+
+
+
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(getApplicationContext(), items));
     }
 
-//    private void replaceFragment(Fragment fragment){
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.replace(R.id.frame_layout, fragment);
-//        fragmentTransaction.commit();
-//    }
 }
+
+
